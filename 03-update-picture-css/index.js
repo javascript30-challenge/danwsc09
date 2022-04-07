426188112dc890217cff172685143d7087278eb4
Wonsang <temporaryaion1@hotmail.com>
2022-04-07 22:57:52 +0900

    find diff b/w FF/Chrome/Safari in file upload and cancel

--- 03-update-picture-css/index.js
+++ 03-update-picture-css/index.js
@@ -20,3 +20,24 @@ inputs.forEach(input => {
   input.addEventListener('change', handleUpdate)
   input.addEventListener('mousemove', handleUpdate)
 })
+
+/**
+ * Firefox vs Chrome vs Safari
+ * 둘다 input type = file 에서 이미지를 하나 선택하고 ok 누르고, 다시 선택하기 누르고 취소를 누른다
+ * - FF, Safari: 기존 파일이 그대로 있음 (event.target.files.length = 1)
+ * - Chrome: 기존 파일이 사라짐 (event.target.files.length = 0)
+ */
+
+const image = document.querySelector('img')
+const file = document.getElementById('file')
+let url
+file.addEventListener('input', e => {
+  console.log('e.target.files', e.target.files)
+  if (e.target.files.length > 0) {
+    url = URL.createObjectURL(e.target.files[0])
+    image.setAttribute('src', url)
+    // URL.revokeObjectURL(url)
+  } else if (url) {
+    image.setAttribute('src', '')
+  }
+})
